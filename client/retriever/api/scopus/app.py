03d85f7dc7bee10deec6a6f4f3da5325841f5d71
damdam166 <dericoo166@gmail.com>
2025-07-09 16:22:16 +0200

    feat(api): merging scopus to the main module.

---
+++ client/retriever/api/scopus/app.py
@@ -0,0 +1,44 @@
+from generic_app import Service
+
+import re
+import httpx
+import json
+
+import pybliometrics
+
+
+class ScopusClient(Service):
+    def __init__(self, apiurl: str = None, apikey: str = None,
+                 mailto: str = None, timeout: int = 20):
+
+        self.name = "Pybliometrics(Scopus)"
+        super().__init__(apiurl=apiurl, apikey=apikey,
+                         mailto=mailto, timeout=timeout)
+
+        pybliometrics.scopus.utils.init(keys=[apikey])
+
+    def query(self, query: str) -> str:
+        """
+        :param query: `Title, author, DOI, ORCID iD, etc..`
+        :return: the result of ``pybliometrics.scopus.*()``. It is various *json*.
+                 the result is a string from `json.dumps()`.
+        """
+
+        query: str = query
+        refresh: bool | int = False
+        verbose: bool = True
+        download: bool = False
+        integrity_fields: list[str] | tuple(str) = None
+        integrity_action: str = 'raise'
+
+        result = pybliometrics.scopus.AuthorSearch(
+            query=query,
+            refresh=refresh,
+            verbose=verbose,
+            download=download,
+            integrity_fields=integrity_fields,
+            integrity_action=integrity_action
+        )
+
+        return result.__str__()
+
