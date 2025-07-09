458abacf481c5c9f0105beae9a7ded8e765355aa
damdam166 <dericoo166@gmail.com>
2025-07-09 16:07:36 +0200

    feat(config): changing the port of the backend.

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
 
 // require('dotenv').config();
-export const socket = io.connect('http://localhost:5005');
+export const socket = io.connect('http://localhost:5001');
 
