6c1c51e96b7a8cf364b316ce67aa995e7eb4e8e0
damdam166 <dericoo166@gmail.com>
2025-07-11 14:13:04 +0200

    feat(dataset): adding the environment variables for the dataset client.

---
+++ dataset/config.py
@@ -0,0 +1,30 @@
+# dataset/config.py
+
+# <Environment variables>
+import os
+from dotenv import load_dotenv, dotenv_values
+
+load_dotenv()
+
+# <Retriever>
+RETRIEVER_HOST: int = int(os.getenv("RETRIEVER_HOST"))
+RETRIEVER_PORT: str = os.getenv("RETRIEVER_PORT")
+RETRIEVER_SCHEME: str = os.getenv("RETRIEVER_SCHEME")
+
+RETRIEVER_URL: str = RETRIEVER_SCHEME +\
+                     RETRIEVER_HOST +\
+               ":" + RETRIEVER_PORT
+# </Retriever>
+
+# <Classifier>
+CLASSIFIER_HOST: int = int(os.getenv("CLASSIFIER_HOST"))
+CLASSIFIER_PORT: str = os.getenv("CLASSIFIER_PORT")
+CLASSIFIER_SCHEME: str = os.getenv("CLASSIFIER_SCHEME")
+
+CLASSIFIER_URL: str = CLASSIFIER_SCHEME +\
+                     CLASSIFIER_HOST +\
+               ":" + CLASSIFIER_PORT
+# </Classifier>
+
+# </Environment variables>
+
