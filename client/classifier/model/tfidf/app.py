279304bd79f75d886210c5c7156fcfcbc9b2ce8c
damdam166 <dericoo166@gmail.com>
2025-08-19 09:57:33 +0200

    feat(classifier): tfidf plan.

--- client/classifier/model/tfidf/app.py
+++ client/classifier/model/tfidf/app.py
@@ -27,6 +27,40 @@ class Tfidf(Service):
 
         return self.generic_prompt(func_prompt, prompt)
 
+    def train(self, output_file: str = "./model.pt", input_file: str) -> None:
+        """
+        A Function to train the model.
+
+        :param output_file: The path of the file that will store the model.
+        :param input_file: The path of the file with the labelled metadata.
+            This one is a json file like this:
+
+            ```
+            {
+            'label_name (example: challenges, themes, etc..)': [
+                    { 'category': "tech", 'text': "Once upon a time..." },
+                    { 'category': "business", 'text': "In the Wilderness..." },
+                    { 'category': "sport", 'text': "By Hugo Montenegro..." },
+                    { 'category': "sport", 'text': "And Sergio Leone..." },
+                    { 'category': "entertainment", 'text': "And Me..." }
+                ]
+            }
+            ```
+
+        Train data percentage: 70\%.
+        Test data percentage: 30\%.
+
+        Steps:
+            1. Clean the texts: add 'text_clean' to each dictionary,
+                it is text but tokenized, stemmed, lemmatisated.
+            2. Train using logistics regression.
+            The model is a pipeline consisting of vectorization and classifier.
+            3. Display the top words for each category and the precision
+                on train and test datasets, and the taken time to train.
+        """
+
+
+
 #############################################################################
 
 
