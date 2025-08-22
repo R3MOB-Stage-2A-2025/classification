77ffe0c359fbc0e4da26ba0b2029d95141346336
damdam166 <dericoo166@gmail.com>
2025-08-23 01:44:34 +0200

    feat(retriever): add a way to personalize more variables for Flask.

--- client/retriever/config.py
+++ client/retriever/config.py
@@ -7,9 +7,13 @@ from dotenv import load_dotenv, dotenv_values
 load_dotenv()
 
 # <Flask + gevent + socketio>
-BACKEND_PORT: int = int(os.getenv("BACKEND_PORT"))
-BACKEND_SECRETKEY: str = os.getenv("BACKEND_SECRETKEY")
-FRONTEND_HOST: str = os.getenv("FRONTEND_HOST")
+FLASK_BACKEND_PORT: int = int(os.getenv("FLASK_BACKEND_PORT"))
+FLASK_BACKEND_SECRETKEY: str = os.getenv("FLASK_BACKEND_SECRETKEY")
+FLASK_FRONTEND_HOST: str = os.getenv("FLASK_FRONTEND_HOST")
+FLASK_DEBUG: bool=\
+    True if os.getenv("FLASK_DEBUG") == "TRUE" else False
+FLASK_ALLOWED_ORIGINS: str = os.getenv("FLASK_ALLOWED_ORIGINS")
+MAX_WORKERS: int = 4
 # </Flask + gevent + socketio>
 
 # <OpenAlex>
