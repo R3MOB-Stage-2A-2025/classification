2d787dfe051a22526b91e0cc7573e95e9ace320e
damdam166 <dericoo166@gmail.com>
2025-09-04 00:15:51 +0200

    feat(dataset): adding sockets for both dev and prod environments.

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -3,10 +3,12 @@ import io from 'socket.io-client';
 // require('dotenv').config();
 
 // <Retriever>
-const socket_retriever = io.connect('wss://localhost:5022', {
-    path: "/socket.io/",
-    transports: [ "websocket" ],
-});
+//const socket_retriever = io.connect('wss://localhost:5022', {
+    //path: "/socket.io/",
+    //transports: [ "websocket" ],
+//});
+
+const socket_retriever = io.connect('ws://localhost:5001');
 // </Retriever>
 
 // <Classifier>
@@ -14,6 +16,8 @@ const socket_classifier = io.connect('wss://localhost:5023', {
     path: "/socket.io/",
     transports: [ "websocket" ],
 });
+
+//const socket_classifier = io.connect('ws://localhost:5011');
 // </Classifier>
 
 export { socket_retriever, socket_classifier };
