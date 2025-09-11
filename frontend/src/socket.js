f144e9f93d081d8db7182921f172a13167121702
damdam166 <dericoo166@gmail.com>
2025-09-11 19:33:16 +0200

    fix(frontend): use the wss instead of ws.

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -3,21 +3,21 @@ import io from 'socket.io-client';
 // require('dotenv').config();
 
 // <Retriever>
-//const socket_retriever = io.connect('wss://localhost:5022', {
-    //path: "/socket.io/",
-    //transports: [ "websocket" ],
-//});
+const socket_retriever = io.connect('wss://localhost:5022', {
+    path: "/socket.io/",
+    transports: [ "websocket" ],
+});
 
-const socket_retriever = io.connect('ws://localhost:5001');
+//const socket_retriever = io.connect('ws://localhost:5001');
 // </Retriever>
 
 // <Classifier>
-//const socket_classifier = io.connect('wss://localhost:5023', {
-    //path: "/socket.io/",
-    //transports: [ "websocket" ],
-//});
+const socket_classifier = io.connect('wss://localhost:5023', {
+    path: "/socket.io/",
+    transports: [ "websocket" ],
+});
 
-const socket_classifier = io.connect('ws://localhost:5011');
+//const socket_classifier = io.connect('ws://localhost:5011');
 // </Classifier>
 
 export { socket_retriever, socket_classifier };
